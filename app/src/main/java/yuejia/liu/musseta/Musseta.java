d4ab9c4cb5be113fd136f9d0329d690a13b29044
longkai <im.longkai@gmail.com>
2015-11-07 03:13:27 +0800

    Upgrade Timber to 3.1.0.
    Note: the latest version of timber is 4.1.0, however it requires  api level >= 15.
    
    Maybe someday we' ll upgrade it someday.

--- app/src/main/java/yuejia/liu/musseta/Musseta.java
+++ app/src/main/java/yuejia/liu/musseta/Musseta.java
@@ -1,12 +1,13 @@
 package yuejia.liu.musseta;
 
 import android.app.Application;
+import android.util.Log;
+
 import com.crashlytics.android.Crashlytics;
 import dagger.ObjectGraph;
 import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 import yuejia.liu.musseta.arch.AndroidModule;
-import yuejia.liu.musseta.helper.CrashlyticsTree;
 
 /**
  * Musseta app.
@@ -21,7 +22,15 @@ public class Musseta extends Application {
     if (BuildConfig.DEBUG) {
       Timber.plant(new Timber.DebugTree());
     } else {
-      Timber.plant(new CrashlyticsTree());
+      Timber.plant(new Timber.Tree() {
+        @Override protected void log(int priority, String tag, String message, Throwable t) {
+          if (priority == Log.WARN) {
+            Crashlytics.log(priority, tag, message);
+          } else if (priority >= Log.ERROR) {
+            Crashlytics.logException(t);
+          }
+        }
+      });
       Fabric.with(this, new Crashlytics());
     }
 
